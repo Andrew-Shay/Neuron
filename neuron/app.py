4c87710ae923006dab151a5cb1310d5fcb39ad43
Andrew Shay <andrew.shay@andrewshay.me>
2017-05-28 01:13:38 -0400

    wx py3

---
+++ neuron/app.py
@@ -0,0 +1,78 @@
+# -----------------------------------
+#     app.py
+#     version: 1.0.0
+#     Author: Andrew Shay
+#     Created: December 10 2015
+#     Description: The Flask server that powers Neuron
+# -----------------------------------
+
+
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter.filedialog import FileDialog
+import base64
+import json
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+files = None
+
+
+@app.before_request
+def before_request():
+    pass
+
+
+@app.teardown_request
+def teardown_request(exception):
+    pass
+
+
+# @app.route('/open_folder/', methods=['GET'])
+# def add_folder():
+#     root = tk.Tk()
+#     root.withdraw()
+#     root.overrideredirect(True)
+#     root.geometry('0x0+0+0')
+#     root.deiconify()
+#     root.lift()
+#     root.focus_force()
+#     file_path = tkFileDialog.askdirectory()
+#     root.destroy()
+#
+#     print("FILEPATH: " + file_path)
+#
+#     return file_path
+
+
+@app.route('/', methods=['GET'])
+def index():
+    random_json = ["Data One", "Data Two", "Data Three", "Data Four"]
+    random_json = json.dumps(random_json)
+    random_json = base64.b64encode(random_json.encode())
+    return render_template('index.html', random_json=random_json)
+
+
+@app.route('/about/', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+
+@app.route('/save_data/', methods=['POST'])
+def save_data():
+    random_json = request.form["random_json"]
+    random_json = base64.b64decode(random_json)
+    random_json = json.loads(random_json)
+
+    for data in random_json:
+        print(data)
+
+    return "Success"
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000, debug=True, use_reloader=True)
